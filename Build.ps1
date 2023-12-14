f8316906b1a7e51721737542fcb9852a149048a3
DavidCarbon <open-source@davidcarbon.dev>
2023-12-14 01:41:16 -0800

    Fix Build Script for 'GameLauncher'
    Solution Project "GameLauncher:" wasn't included originally when updating the version file

--- Build.ps1
+++ Build.ps1
@@ -122,122 +122,133 @@ catch
     exit 1
 }
 
-try
+function Update-Version 
 {
-	if (!($EnvironmentInt -eq 2))
+    param (
+        [int]$LiveEnvironmentStatus,
+        [string]$LiveFilePath
+    )
+
+	#Get Path to csproj
+	$path = $LiveFilePath
+
+	#Read csproj (XML)
+	$xml = [xml](Get-Content $path)
+
+	#Retrieve Version Nodes
+
+	#Version Major
+	$versionMajor = $xml.Project.PropertyGroup.VersionMajor
+	$versionMajorUpgrade = $xml.Project.PropertyGroup.VersionMajorUpgrade
+
+	#Version Minor
+	$versionMinor = $xml.Project.PropertyGroup.VersionMinor
+	$versionMinorUpgrade = $xml.Project.PropertyGroup.VersionMinorUpgrade
+
+	#Version Build
+	$versionBuild = $xml.Project.PropertyGroup.VersionBuild
+
+	#Increment Build Version - Will reset all sub nodes
+
+	#Create Temporary Boolean for String Null Checks
+	$tempBool = $null
+
+	#Convert String to Booleans
+	if ([bool]::TryParse($versionMajorUpgrade, [ref]$tempBool)) 
 	{
-		#Get Path to csproj
-		$path = "$PSScriptRoot\SBRW.Launcher.Net\SBRW.Launcher.csproj"
+		# parsed to a boolean
+		$versionMajorUpgrade = $tempBool
+	}
+	else
+	{
+		$versionMajorUpgrade = $false
+	}
 
-		#Read csproj (XML)
-		$xml = [xml](Get-Content $path)
+	$tempBool = $null
 
-		#Retrieve Version Nodes
+	if ([bool]::TryParse($versionMinorUpgrade, [ref]$tempBool)) 
+	{
+		# parsed to a boolean
+		$versionMinorUpgrade = $tempBool
+	}
+	else
+	{
+		$versionMinorUpgrade = $false
+	}
 
-		#Version Major
-		$versionMajor = $xml.Project.PropertyGroup.VersionMajor
-		$versionMajorUpgrade = $xml.Project.PropertyGroup.VersionMajorUpgrade
+	$tempBool = $null
 
-		#Version Minor
-		$versionMinor = $xml.Project.PropertyGroup.VersionMinor
-		$versionMinorUpgrade = $xml.Project.PropertyGroup.VersionMinorUpgrade
-
-		#Version Build
-		$versionBuild = $xml.Project.PropertyGroup.VersionBuild
-
-		#Increment Build Version - Will reset all sub nodes
-
-		#Create Temporary Boolean for String Null Checks
-		$tempBool = $null
-
-		#Convert String to Booleans
-		if ([bool]::TryParse($versionMajorUpgrade, [ref]$tempBool)) 
+	#Update Version Numbers & Put new version back into csproj (XML)
+	if ($versionMajorUpgrade)
+	{
+		$xml.Project.PropertyGroup.VersionMajor = ([Convert]::ToInt32($versionMajor,10)+1).ToString()
+		$xml.Project.PropertyGroup.VersionMajorUpgrade = "false"
+		$xml.Project.PropertyGroup.VersionMinor = "0"
+		$xml.Project.PropertyGroup.VersionMinorUpgrade = "false"
+		$xml.Project.PropertyGroup.VersionBuild = "0"
+	}
+	else
+	{
+		if ($versionMinorUpgrade)
 		{
-			# parsed to a boolean
-			$versionMajorUpgrade = $tempBool
-		}
-		else
-		{
-			$versionMajorUpgrade = $false
-		}
-
-		$tempBool = $null
-
-		if ([bool]::TryParse($versionMinorUpgrade, [ref]$tempBool)) 
-		{
-			# parsed to a boolean
-			$versionMinorUpgrade = $tempBool
-		}
-		else
-		{
-			$versionMinorUpgrade = $false
-		}
-
-		$tempBool = $null
-
-		#Update Version Numbers & Put new version back into csproj (XML)
-		if ($versionMajorUpgrade)
-		{
-			$xml.Project.PropertyGroup.VersionMajor = ([Convert]::ToInt32($versionMajor,10)+1).ToString()
-			$xml.Project.PropertyGroup.VersionMajorUpgrade = "false"
-			$xml.Project.PropertyGroup.VersionMinor = "0"
+			$xml.Project.PropertyGroup.VersionMinor = ([Convert]::ToInt32($versionMinor,10)+1).ToString()
 			$xml.Project.PropertyGroup.VersionMinorUpgrade = "false"
 			$xml.Project.PropertyGroup.VersionBuild = "0"
 		}
 		else
 		{
-			if ($versionMinorUpgrade)
+			# Check if VersionBuild is even or odd
+			$versionBuildInt = [Convert]::ToInt32($versionBuild, 10)
+
+			if ($LiveEnvironmentStatus -eq 0 -and $versionBuildInt % 2 -eq 0) 
 			{
-				$xml.Project.PropertyGroup.VersionMinor = ([Convert]::ToInt32($versionMinor,10)+1).ToString()
-				$xml.Project.PropertyGroup.VersionMinorUpgrade = "false"
-				$xml.Project.PropertyGroup.VersionBuild = "0"
+				# Even number, increment to the next even number (Should be on RELEASE MODE)
+				$versionBuildInt += 2
 			}
 			else
 			{
-				# Check if VersionBuild is even or odd
-				$versionBuildInt = [Convert]::ToInt32($versionBuild, 10)
-
-				if ($EnvironmentInt -eq 0 -and $versionBuildInt % 2 -eq 0) 
-				{
-					# Even number, increment to the next even number (Should be on RELEASE MODE)
-					$versionBuildInt += 2
-				}
-				else
-				{
-					# Odd number, increment to the next odd number (Should be on DEBUG MODE)
-					$versionBuildInt++
-				}
-
-				$xml.Project.PropertyGroup.VersionBuild = $versionBuildInt.ToString()
+				# Odd number, increment to the next odd number (Should be on DEBUG MODE)
+				$versionBuildInt++
 			}
+
+			$xml.Project.PropertyGroup.VersionBuild = $versionBuildInt.ToString()
 		}
+	}
 
-		# File Save with Formatting Settings
-		try
-		{
-			# Save csproj (XML) with preserving formatting
-			$settings = New-Object System.Xml.XmlWriterSettings
-			$settings.Indent = $true
-			$settings.IndentChars = "    "  # You can adjust the number of spaces for indentation
+	# File Save with Formatting Settings
+	try
+	{
+		# Save csproj (XML) with preserving formatting
+		$settings = New-Object System.Xml.XmlWriterSettings
+		$settings.Indent = $true
+		$settings.IndentChars = "    "  # You can adjust the number of spaces for indentation
 
-			# Create a StringWriter to preserve formatting
-			$stringWriter = New-Object System.IO.StringWriter
-			$xmlWriter = [System.Xml.XmlWriter]::Create($stringWriter, $settings)
+		# Create a StringWriter to preserve formatting
+		$stringWriter = New-Object System.IO.StringWriter
+		$xmlWriter = [System.Xml.XmlWriter]::Create($stringWriter, $settings)
 
-			# Save XML (Cache) and Cleanup
-			$xml.Save($xmlWriter)
-			$xmlWriter.Close()
+		# Save XML (Cache) and Cleanup
+		$xml.Save($xmlWriter)
+		$xmlWriter.Close()
 
-			#Save csproj (Formatted XML)
-			$stringWriter.ToString() | Out-File $path -NoNewline
+		#Save csproj (Formatted XML)
+		$stringWriter.ToString() | Out-File $path -NoNewline
 
-			echo ([string]::Format("========== New Version Number: {0}.{1}.{2} ==========",$xml.Project.PropertyGroup.VersionMajor,$xml.Project.PropertyGroup.VersionMinor,$xml.Project.PropertyGroup.VersionBuild))
-		}
-		catch
-		{
-			throw $_
-			exit 1
-		}
+		echo ([string]::Format("========== New Version Number: {0}.{1}.{2} ==========",$xml.Project.PropertyGroup.VersionMajor,$xml.Project.PropertyGroup.VersionMinor,$xml.Project.PropertyGroup.VersionBuild))
+	}
+	catch
+	{
+		throw $_
+		exit 1
+	}
+}
+
+try
+{
+	if (!($EnvironmentInt -eq 2))
+	{
+		Update-Version -LiveEnvironmentStatus $EnvironmentInt -LiveFilePath "$PSScriptRoot\SBRW.Launcher.Net\SBRW.Launcher.csproj"
+		Update-Version -LiveEnvironmentStatus $EnvironmentInt -LiveFilePath "$PSScriptRoot\GameLauncher\GameLauncher.csproj"
 	}
 	else
 	{
